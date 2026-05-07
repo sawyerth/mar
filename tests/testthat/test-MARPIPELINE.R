a63dcbdc65c026d9bac2df4cf4fb1b59a38f3673
sawyerth <sawyerthomsen15@berkeley.edu>
2026-05-07 14:42:44 -0700

    inital vignette

--- tests/testthat/test-MARPIPELINE.R
+++ tests/testthat/test-MARPIPELINE.R
@@ -86,3 +86,25 @@ test_that("MARPIPELINE runs with missing genotype data (extdata)", {
         )
     )
 })
+
+test_that("MARPIPELINE for data reproducability", {
+    name <- "extdata_example"
+    workdir <- tempfile("marpipeline_extdata_")
+    dir.create(workdir)
+    on.exit(cleanup_workdir(workdir), add = TRUE)
+
+    genofile <- system.file("extdata", "genome.tsv", package = "mar")
+    lonlatfile <- system.file("extdata", "lonlat.csv", package = "mar")
+
+    oldwd <- getwd()
+    on.exit(setwd(oldwd), add = TRUE)
+
+    expect_invisible(MARPIPELINE(
+        name = name,
+        workdir = workdir,
+        genofile = genofile,
+        lonlatfile = lonlatfile,
+        filetype = "text",
+        randseed = 91
+    ))
+})
